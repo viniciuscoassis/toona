605300eb264d8a18d40cc1c3aa104fef12f7b8b4
Vinicius Assis <106977892+viniciuscoassis@users.noreply.github.com>
2024-11-07 13:55:18 -0300

    feat: add hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,73 @@
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faTelegram, faDiscord } from '@fortawesome/free-brands-svg-icons';
-import { faChartLine, faPaintBrush } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTelegram,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+import { faChartLine, faPaintBrush } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div className="header">
-        <img src="/logo.png" alt="Toona Image 1" className="main-image" style={{ width: '50%'}} />
-        <img src="/fam.jpeg" alt="Toona Image 2" className="main-image" style={{ width: '50%' }} />
+        <img
+          src="/logo.png"
+          alt="Toona Image 1"
+          className="main-image"
+          style={{ width: "50%" }}
+        />
+        <img
+          src="/fam.jpeg"
+          alt="Toona Image 2"
+          className="main-image"
+          style={{ width: "50%" }}
+        />
       </div>
       {/* <h1>Welcome to Toona</h1> */}
-      <button className="buy-button ">Buy</button>
+      <button
+        className="buy-button"
+        onClick={() =>
+          window.open(
+            "https://degen.express/fantom/token/0x7F19C5adB254601178BeBcC5353A719dbc1B119A",
+            "_blank"
+          )
+        }
+      >
+        Buy
+      </button>
       <div className="social-buttons">
-        <a href="https://twitter.com/toona" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://twitter.com/toona"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <button className="social-button">
             <FontAwesomeIcon icon={faTwitter} />
           </button>
         </a>
-        <a href="https://telegram.org/toona" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://t.me/@toonasonic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <button className="social-button">
             <FontAwesomeIcon icon={faTelegram} />
           </button>
         </a>
-        <a href="https://paintswap.finance/marketplace/fantom/collections/0x480087cf04c245bd24ffb4495010ef280cbdf1ef/nfts" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://paintswap.finance/marketplace/fantom/collections/0x480087cf04c245bd24ffb4495010ef280cbdf1ef/nfts"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <button className="social-button">
             <FontAwesomeIcon icon={faPaintBrush} />
           </button>
         </a>
-        <a href="https://dexscreener.com/toona" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://dexscreener.com/fantom/0x7F19C5adB254601178BeBcC5353A719dbc1B119A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <button className="social-button">
             <FontAwesomeIcon icon={faChartLine} />
           </button>
